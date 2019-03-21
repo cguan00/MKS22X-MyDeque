18ccc6fb47b29866013f9a0c0cdffc96e09a13ec
Christy Guan <cguan00@stuy.edu>
2019-03-21 14:26:57 -0400

    created constructors, size, and toString

--- MyDeque.java
+++ MyDeque.java
@@ -2,14 +2,34 @@ public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
-  public MyDeque(){  }
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
+  @SuppressWarnings("unchecked")
+  public MyDeque(){
+    data = (E[])new Object[10];
+  }
+
+  @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity){
+    data = (E[])new Object[initialCapacity];
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public String toString(){
+    String output = "";
+    return output;
+  }
+
   public void addFirst(E element){ }
+
   public void addLast(E element){ }
+
   public E removeFirst(){ }
+
   public E removeLast(){ }
+
   public E getFirst(){ }
+
   public E getLast(){ }
 }
