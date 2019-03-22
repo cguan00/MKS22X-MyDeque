b7c88733857e4c6b922113962a0915e51db83de4
Christy Guan <cguan00@stuy.edu>
2019-03-22 14:24:11 -0400

    added exception throwing

--- MyDeque.java
+++ MyDeque.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
@@ -5,17 +7,11 @@ public class MyDeque<E>{
   @SuppressWarnings("unchecked")
   public MyDeque(){
     data = (E[])new Object[10];
-    size = data.length;
-    start = 0;
-    end = 1;
   }
 
   @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
     data = (E[])new Object[initialCapacity];
-    size = initialCapacity;
-    start = 0;
-    end = 1;
   }
 
   public int size(){
@@ -31,37 +27,60 @@ public class MyDeque<E>{
   }
 
   public void addFirst(E element){
+    if(element == null){
+      throw new NullPointerException();//throw exception if specified element is null
+    }
     if(start - 1 > 0){//if not past the beginning of data array
       data[start - 1] = element;
     } else{
 
     }
+    size++;//increase size by 1
   }
 
   public void addLast(E element){
+    if(element == null){
+      throw new NullPointerException();//throw exception if specified element is null
+    }
     if(end + 1 < data.length - 1){//if not past the end of data array
       data[end + 1] = element;
     } else{
 
     }
-
+    size++;//decrease size by 1
   }
 
-  // public E removeFirst(){
-  //
-  // }
-  //
-  // public E removeLast(){
-  //
-  // }
+  public E removeFirst(){
+    // if(size() == 0){//throw exception if deque is empty
+    //    throw new NoSuchElementException();
+    // }
+    return data[start];
+  }
+
+  public E removeLast(){
+    // if(size() == 0){//throw exception if deque is empty
+    //    throw new NoSuchElementException();
+    // }
+    return data[end];
+  }
 
   public E getFirst(){
+    if(size() == 0){//throw exception if deque is empty
+       throw new NoSuchElementException();
+    }
     return data[start];
   }
 
   public E getLast(){
+    if(size() == 0){//throw exception if deque is empty
+       throw new NoSuchElementException();
+    }
     return data[end];
   }
 
+  @SuppressWarnings("unchecked")
+  public void resize(){
+    data = (E[])new Object[size() * 2 + 1];
+  }
 
 }
