ec5e48a0376495e0fed4ad6b2fe90c66fdefe689
Christy Guan <cguan00@stuy.edu>
2019-03-22 14:44:32 -0400

    start testing methods

--- Driver.java
+++ Driver.java
@@ -1,6 +1,8 @@
 public class Driver{
   public static void main(String[] args){
-    // MyDeque<Integer> deque1 = new MyDeque();
-    // System.out.println(deque1);
+    MyDeque<Integer> deque1 = new MyDeque<>();
+    System.out.println(deque1);
+    deque1.addFirst(5);
+    System.out.println(deque1);
   }
 }
