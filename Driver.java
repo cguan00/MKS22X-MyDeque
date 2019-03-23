141354ba87e565a8ef431a1c32588b4bd8e07fa3
Christy Guan <cguan00@stuy.edu>
2019-03-23 18:37:08 -0400

    tested methods

--- Driver.java
+++ Driver.java
@@ -3,6 +3,8 @@ public class Driver{
     MyDeque<Integer> deque1 = new MyDeque<>();
     System.out.println(deque1);
     deque1.addFirst(5);
+    deque1.addFirst(10);
+    System.out.println(deque1.size());
     System.out.println(deque1);
   }
 }
