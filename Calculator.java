8c4891cc649b1bf56baacc0761b7ff6dafe3114c
Christy Guan <cguan00@stuy.edu>
2019-03-25 14:47:38 -0400

    convert string into tokens with split

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,18 @@
 public class Calculator{
-
   /*Evaluate a postfix expression stored in s.
    *Assume valid postfix notation, separated by spaces.
    */
   public static double eval(String s){
+    String[] data = s.split(" ");//convert string into tokens
+
+    //printing out data array to check if splitting correctly
+    // for(int i = 0; i < data.length; i++){
+    //   System.out.println(data[i]);
+    // }
+
     return 0.0;
   }
+
+
+
 }
