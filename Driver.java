2d77e3a5f7d4f3d0af2699ddf2437087d09d8766
Christy Guan <cguan00@stuy.edu>
2019-03-25 14:46:43 -0400

    copied test cases from website

--- Driver.java
+++ Driver.java
@@ -1,10 +1,22 @@
 public class Driver{
   public static void main(String[] args){
-    MyDeque<Integer> deque1 = new MyDeque<>();
-    System.out.println(deque1);
-    deque1.addFirst(5);
-    deque1.addFirst(10);
-    System.out.println(deque1.size());
-    System.out.println(deque1);
+    // MyDeque<Integer> deque1 = new MyDeque<>();
+    // System.out.println(deque1);
+    // deque1.addFirst(5);
+    // deque1.addFirst(10);
+    // System.out.println(deque1.size());
+    // System.out.println(deque1);
+
+    Calculator.eval("10 2.0 +");//should be 12.0
+    System.out.println("");
+
+    Calculator.eval("11 3 - 4 + 2.5 *");//should be 30.0
+    System.out.println("");
+
+    Calculator.eval("8 2 + 99 9 - * 2 + 9 -");//should be 893.0
+    System.out.println("");
+
+    Calculator.eval("1 2 3 4 5 + * - -");//should be 26.0
+    System.out.println("");
   }
 }
