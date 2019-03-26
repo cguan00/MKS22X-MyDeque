0d5dfc225bd784a20c24ad8b968d30755ca93adf
Christy Guan <cguan00@stuy.edu>
2019-03-26 14:45:43 -0400

    simple test

--- Driver.java
+++ Driver.java
@@ -7,16 +7,19 @@ public class Driver{
     // System.out.println(deque1.size());
     // System.out.println(deque1);
 
-    Calculator.eval("10 2.0 +");//should be 12.0
+    Calculator.eval("1 2.0 +");//should be 12.0
     System.out.println("");
 
-    Calculator.eval("11 3 - 4 + 2.5 *");//should be 30.0
-    System.out.println("");
-
-    Calculator.eval("8 2 + 99 9 - * 2 + 9 -");//should be 893.0
-    System.out.println("");
-
-    Calculator.eval("1 2 3 4 5 + * - -");//should be 26.0
-    System.out.println("");
+    // Calculator.eval("10 2.0 +");//should be 12.0
+    // System.out.println("");
+    //
+    // Calculator.eval("11 3 - 4 + 2.5 *");//should be 30.0
+    // System.out.println("");
+    //
+    // Calculator.eval("8 2 + 99 9 - * 2 + 9 -");//should be 893.0
+    // System.out.println("");
+    //
+    // Calculator.eval("1 2 3 4 5 + * - -");//should be 26.0
+    // System.out.println("");
   }
 }
