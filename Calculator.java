a9c6c59c2951a8215992603912eda78810eb811b
Christy Guan <cguan00@stuy.edu>
2019-03-26 14:46:12 -0400

    worked on adding and working with stack:

--- Calculator.java
+++ Calculator.java
@@ -5,10 +5,57 @@ public class Calculator{
   public static double eval(String s){
     String[] data = s.split(" ");//convert string into tokens
 
-    //printing out data array to check if splitting correctly
+    // printing out data array to check if splitting correctly
     // for(int i = 0; i < data.length; i++){
     //   System.out.println(data[i]);
     // }
+
+    MyDeque<Double> stack = new MyDeque<>();
+
+    // MyDeque<String> stack1 = new MyDeque<>();
+    //
+    // for(int i = 0; i < data.length; i++){
+    //   stack1.addLast(data[i]);
+    // }
+    // System.out.println(stack1);
+
+    for(int i = 0; i < data.length; i++){
+      // if(!data[i].equals("+") && !data[i].equals("-") && !data[i].equals("*") && !data[i].equals("/")){
+      //   stack.addLast(Double.valueOf(data[i]));
+      // }
+      // if(data[i].equals("+")){
+      //   Double first = Double.valueOf(data[i - 1]);
+      //   Double second = Double.valueOf(data[i - 2]);
+      //   // System.out.println(first + second);
+      //   stack.addLast(first + second);
+      //   // System.out.println(stack);
+      // }
+
+      if(data[i].equals("+")){
+        Double first = stack.removeLast();
+        Double second = stack.removeLast();
+        stack.addLast(first + second);
+        // System.out.println(first + second);
+        System.out.println("add");
+        // System.out.println(stack);
+      } else if(data[i].equals("-")){
+        Double first = stack.removeLast();
+        Double second = stack.removeLast();
+        stack.addLast(second - first);
+      }else if(data[i].equals("*")){
+        Double first = stack.removeLast();
+        Double second = stack.removeLast();
+        stack.addLast(first * second);
+      } else if(data[i].equals("-")){
+        Double first = stack.removeLast();
+        Double second = stack.removeLast();
+        stack.addLast(second / first);
+      } else{
+        stack.addLast(Double.valueOf(data[i]));
+      }
+    }
+
+    System.out.println(stack);
 
     return 0.0;
   }
