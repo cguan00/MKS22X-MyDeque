0c543d92484c7c574c28457b557a0206b83b00c7
Christy Guan <cguan00@stuy.edu>
2019-03-27 17:30:30 -0400

    deleted print statements used for debugging and added comments

--- Calculator.java
+++ Calculator.java
@@ -12,38 +12,28 @@ public class Calculator{
 
     MyDeque<Double> stack = new MyDeque<>();
 
-    for(int i = 0; i < data.length; i++){
-      if(data[i].equals("+")){
-        Double first = stack.removeLast();
-        Double second = stack.removeLast();
-        stack.addLast(first + second);
-        // System.out.println(first + second);
-        // System.out.println("add");
-        // System.out.println(stack);
-      }else if(data[i].equals("-")){
-        Double first = stack.removeLast();
-        Double second = stack.removeLast();
-        stack.addLast(second - first);
-      }else if(data[i].equals("*")){
-        Double first = stack.removeLast();
-        Double second = stack.removeLast();
-        stack.addLast(first * second);
-      }else if(data[i].equals("/")){
-        Double first = stack.removeLast();
-        Double second = stack.removeLast();
-        stack.addLast(second / first);
+    for(int i = 0; i < data.length; i++){//loop through the tokens in the data array
+      if(data[i].equals("+")){//if you want to add
+        Double first = stack.removeLast();//number on the right
+        Double second = stack.removeLast();//number on the left
+        stack.addLast(first + second);//add the two numbers together, order doesn't matter
+      }else if(data[i].equals("-")){//if you want to subtract
+        Double first = stack.removeLast();//number on the right
+        Double second = stack.removeLast();//number on the left
+        stack.addLast(second - first);//subtract the number on the right from the one on the left, order does matter
+      }else if(data[i].equals("*")){//if you want to multiply
+        Double first = stack.removeLast();//number on the right
+        Double second = stack.removeLast();//number on the left
+        stack.addLast(first * second);//multiply the two numbers together, order doesn't matter
+      }else if(data[i].equals("/")){//if you want to divide
+        Double first = stack.removeLast();//number on the right
+        Double second = stack.removeLast();//number on the left
+        stack.addLast(second / first);//divide number on left by number on right, order does matter
       } else{
-        // System.out.println("a num");
         stack.addLast(Double.valueOf(data[i]));
-        // System.out.println("num");
-        // System.out.println(stack.size());
-        // System.out.println(stack);
       }
     }
-
     return stack.getFirst();
   }
-
-
 
 }
